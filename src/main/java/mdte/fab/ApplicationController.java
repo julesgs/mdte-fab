ab9b220dac97849a20cbfc9a2280207860d08ffe
Jules Gauzentes <jgauzentes@Interlab15.com>
2025-01-15 09:58:01 +0100

    page propre / mais code à nettoyer

--- src/main/java/mdte/fab/ApplicationController.java
+++ src/main/java/mdte/fab/ApplicationController.java
@@ -5,10 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import service.Modele;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class ApplicationController {
@@ -16,13 +18,16 @@ public class ApplicationController {
     private Button RefreshButton;
 
     @FXML
-    private Label selectOrder_label;
+    private Label selectOrder_label, fabricationOrder_label, option_1_label, option_2_label, option_3_label;
 
     @FXML
     private ListView<String> orders_listView, options_listView, stocks_listView;
 
     @FXML
     private TextField numOrder_field, custommer_field, mdte_field, price_field;
+
+    @FXML
+    private Rectangle rectangle;
 
     private List<Custommer> _custommers;
 
@@ -37,6 +42,14 @@ public class ApplicationController {
     @FXML
     protected void onRefreshButtonClick() {
         selectOrder_label.setVisible(true);
+        fabricationOrder_label.setVisible(false);
+
+        option_1_label.setVisible(false);
+        option_2_label.setVisible(false);
+        option_3_label.setVisible(false);
+        rectangle.setVisible(false);
+
+
         List<Order> orders = modele.getOrder();
         orders_listView.getItems().clear();
         for (Order o : orders) {
@@ -58,11 +71,18 @@ public class ApplicationController {
 
         if (selectedOrder != null) {
             selectOrder_label.setVisible(false);
+
+            fabricationOrder_label.setVisible(true);
+            fabricationOrder_label.setText("Fabrication de la commande " + selectedOrder.getID());
+            option_1_label.setVisible(true);
+            option_2_label.setVisible(true);
+            option_3_label.setVisible(true);
+            rectangle.setVisible(true);
+
         }
 
         List<String> lesOptions = selectedOrder.getOptions();
         options_listView.getItems().clear();
-        //stocks_listView.getItems().clear();
         for (String opt : lesOptions) {
             Options option = modele.getOptionByID(opt);
             try {
@@ -100,12 +120,29 @@ public class ApplicationController {
 
         try {
             List<String> lesOpts = selectedOrder.getOptions();
-            for (String opt : lesOpts) {
+            List<Label> labels = Arrays.asList(option_1_label, option_2_label, option_3_label);
+
+            for (int i = 0; i < lesOpts.size(); i++) {
+                String opt = lesOpts.get(i);
                 Stock s = modele.getStockByRefOption(opt);
-                //stocks_listView.getItems().add(s.getID() + "    =>    " + s.getQuantity() + " pièce(s) disponibles");
+
+                if (i < labels.size()) {
+                    Label currentLabel = labels.get(i);
+                    if (s.getQuantity() > 0) {
+                        currentLabel.setStyle("-fx-text-fill: #24bf00;");
+                        currentLabel.setText(s.getID() + "   -   Disponible");
+                    } else {
+                        currentLabel.setStyle("-fx-text-fill: red;");
+                        currentLabel.setText(s.getID() + "   -   Indisponible");
+                    }
+                }
             }
         } catch (Exception e){
-            //stocks_listView.getItems().add("Erreur lors du chargement des Stocks");
+            option_1_label.setStyle("-fx-text-fill: red;");
+            option_1_label.setText("Erreur lors du chargement des stocks pour la commande " + selectedOrder.getID());
+            option_2_label.setVisible(false);
+            option_3_label.setVisible(false);
+            rectangle.setVisible(false);
         }
     }
 
